4ee47972c409393f03bd58eabc897309431cdc0f
Allan <allankildare27@gmail.com>
2020-08-29 19:00:29 -0300

    videos iniciais atualizados

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import PageDefault from '../../../components/PageDefault'
 import FormField from '../../../components/FormField'
+import Button from '../../../components/Button'
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -9,82 +10,88 @@ function CadastroCategoria() {
     descricao: '',
     cor: '',
   }
-  const [categorias, setCategorias] = useState([])
-  const [values, setValues] = useState(valoresIniciais)
-  
-
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
   function setValue(chave, valor) {
     setValues({
       ...values,
       [chave]: valor,
-    })
+    });
   }
 
   function handleChange(infosDoEvento) {
     setValue(
       infosDoEvento.target.getAttribute('name'),
-      infosDoEvento.target.value
+      infosDoEvento.target.value,
     )
   }
+
+  useEffect (() => {
+    console.log('alo alo')
+  }, [])
 
   return (
-      <PageDefault>
-        <h1>Cadastro de Categoria - {values.nome}</h1>
-          
-        <form onSubmit={function handleSubmit(infosDoEvento) {
-            infosDoEvento.preventDefault()
-            setCategorias([
-              ...categorias,
-              values
-            ])
-            setValues(valoresIniciais)
-          }}>
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria -
+        {values.nome}
+      </h1>
 
-            <FormField 
-              label="Nome da Categoria"
-              type="text"
-              name="nome"
-              value={values.nome}
-              onChange={handleChange}
-            />
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        setCategorias([
+          ...categorias,
+          values,
+        ])
+        setValues(valoresIniciais)
+      }}
+      >
 
-            <div>
-            <label>
-              Descrição:
-              <textarea 
-                type="text"
-                value={values.descricao}
-                name="descricao"
-                onChange={handleChange}
-              />
-            </label>
-            </div>
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-            <FormField 
-            label="Cor"
-              type="color"
-              name="cor"
-              value={values.cor}
-              onChange={handleChange}
-            />
-            
-            <button>Cadastrar</button>
-          </form>
-          <ul>
-              {categorias.map((categoria, indice) => {
-                return (
-                  <li key={`${categoria}${indice}`}>
-                    {categoria.nome}
-                  </li>
-                )
-              })}
-          </ul>
-        <Link to="/cadastro/categoria">
-            Ir para home
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+      <ul>
+        {categorias.map((categoria, indice) => {
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+          )
+        })}
+        </ul>
+
+        <Link to="/">
+          Ir para home
         </Link>
-      </PageDefault> 
-    )
-  }
+        
+      </PageDefault>
+  )
+}
 
-  export default CadastroCategoria
+export default CadastroCategoria
